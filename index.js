0fdcc6e47fa0dfcc4ff39173dff1b5f32f028247
Hozefa <hozefam@gmail.com>
2020-05-24 00:40:07 +0300

    Async Obserable

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import { Observable } from 'rxjs';
 
 const observable = new Observable((subscriber) => {
-  subscriber.next('Hello');
-  subscriber.next('World');
-  subscriber.complete();
-  subscriber.next('Hello Again'); // <-< Will not appear as complete is already called
+  let count = 0;
+
+  setInterval(() => {
+    subscriber.next(count);
+    if (count >= 5) {
+      subscriber.complete();
+    }
+    count = count + 1;
+  }, 1000);
+
+  return () => {
+    console.log('Clean up called!');
+  };
 });
 
 // params are - next, error and complete
+
+console.log('before');
 observable.subscribe(
   (value) => console.log('next', value),
   (err) => console.log('error', err),
   () => console.log('Complete!')
 );
+console.log('after');
