434fc905b2102465608a2ad794c9dd2a0676dc5e
Hozefa <hozefam@gmail.com>
2020-05-24 00:15:02 +0300

    Basic Observable example

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import { Observable } from 'rxjs';
+
+const observer = {
+  next: (value) => console.log('next', value),
+  error: (err) => console.log('error', err),
+  complete: () => console.log('complete'),
+};
+
+const observable = new Observable((subscriber) => {
+  subscriber.next('Hello');
+  subscriber.next('World');
+  subscriber.complete();
+});
+
+observable.subscribe(observer);
